b0e9d04649b1a008e038eb0a53dcb287878c55a2
jvcoutinho <jv.coutinho@victorconsultoria.com.br>
2020-08-19 22:52:15 -0300

    36 - Using the Authentication middleware

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -12,80 +12,73 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AuthController : ControllerBase
-  {
-    private readonly IAuthRepository _repo;
-    private readonly IConfiguration _config;
-    public AuthController(IAuthRepository repo, IConfiguration config)
-    {
-		_config = config;
-		_repo = repo;
-    }
-
-    [HttpPost("register")]
-    public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
-    {
-		// todo validate request
-
-		userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-
-		if (await _repo.UserExists(userForRegisterDto.Username))
-			return BadRequest("Username alredy exists");
-
-		var userToCreate = new User
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IAuthRepository _repo;
+		private readonly IConfiguration _config;
+		public AuthController(IAuthRepository repo, IConfiguration config)
 		{
-			Username = userForRegisterDto.Username
-		};
+			_config = config;
+			_repo = repo;
+		}
 
-		var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
-
-		return StatusCode(201);
-    }
-
-    [HttpPost("login")]
-    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-    {
-		var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
-
-		if (userFromRepo == null)
-			return Unauthorized();
-
-		var claims = new[]
+		[HttpPost("register")]
+		public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
 		{
-			new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-			new Claim(ClaimTypes.Name, userFromRepo.Username)
-    	};
+			// todo validate request
 
+			userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-		var key = new SymmetricSecurityKey(Encoding.UTF8.
-			GetBytes(_config.GetSection("AppSettings:Token").Value));
+			if (await _repo.UserExists(userForRegisterDto.Username))
+				return BadRequest("Username alredy exists");
 
-		var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+			var userToCreate = new User
+			{
+				Username = userForRegisterDto.Username
+			};
 
-		var tokenDescriptor = new SecurityTokenDescriptor
+			var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+			return StatusCode(201);
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
 		{
-			Subject = new ClaimsIdentity(claims),
-			Expires = DateTime.Now.AddDays(1),
-			SigningCredentials = creds
-		};
+			var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
-		var tokenHandler = new JwtSecurityTokenHandler();
+			if (userFromRepo == null)
+				return Unauthorized();
 
-		var token = tokenHandler.CreateToken(tokenDescriptor);
-
-		return Ok(new {
-			token = tokenHandler.WriteToken(token)
-		});
+			var claims = new[]
+			{
+				new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+				new Claim(ClaimTypes.Name, userFromRepo.Username)
+				};
 
 
+			var key = new SymmetricSecurityKey(Encoding.UTF8.
+				GetBytes(_config.GetSection("AppSettings:Token").Value));
 
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(claims),
+				Expires = DateTime.Now.AddDays(1),
+				SigningCredentials = creds
+			};
 
+			var tokenHandler = new JwtSecurityTokenHandler();
 
+			var token = tokenHandler.CreateToken(tokenDescriptor);
 
+			return Ok(new {
+				token = tokenHandler.WriteToken(token)
+			});
 
-    }
-  }
+		}
+	}
 }
