a0f07c00bb0c2fa5290956a2314ae034a9f10e92
jvcoutinho <jv.coutinho@victorconsultoria.com.br>
2020-08-22 18:05:55 -0300

    56. Wrapping 3rd party libraries as an Angular service

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,14 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('register success');
+      this.alertify.success('register success');
     }, err => {
       console.log(err);
     })
@@ -25,7 +26,7 @@ export class RegisterComponent implements OnInit {
 
   cancel() {
     this.cancelRegister.emit();
-    console.log('cancelled');
+    this.alertify.error('cancelled');
   }
 
 }
