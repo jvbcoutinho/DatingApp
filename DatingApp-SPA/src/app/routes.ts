a36e2ab3d85b9b9a50e53ca8e2d751857c24f49c
jvcoutinho <jv.coutinho@victorconsultoria.com.br>
2020-08-23 20:06:18 -0300

    End of section 7

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -4,11 +4,20 @@ import { ListsComponent } from './lists/lists.component'
 import { HomeComponent } from './home/home.component'
 import { MemberListComponent } from './member-list/member-list.component'
 import { AuthGuard } from './_guards/auth.guard'
+import { AuthService } from './_services/auth.service'
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-  { path: 'messages', component: MessagesComponent},
-  { path: 'lists', component: ListsComponent},
-  { path: '**', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent},
+      { path: 'messages', component: MessagesComponent},
+      { path: 'lists', component: ListsComponent},
+    ]
+  },
+
+  { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
